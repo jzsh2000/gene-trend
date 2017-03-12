a576169d4735ce9ad60f9c5e8fb5cf8b9dd95753
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-03-12 22:46:56 +0800

    Minor change
    When deciding the PMID range of each year, use the data of every first
    months

--- script/efetch-pubmed.sh
+++ script/efetch-pubmed.sh
@@ -7,9 +7,9 @@ fi
 
 year=$(basename $outdir | grep -oP '^[0-9]+')
 > ${outdir}/pmid.info
-for var in `seq 1996 $year`
+for var in `seq 1990 $year`
 do
-    esearch -db pubmed -query '("'$var'/01/01"[EDAT] : "'$var'/01/01"[EDAT])' \
+    esearch -db pubmed -query '("'$var'/01/01"[EDAT] : "'$var'/01/31"[EDAT])' \
         | efetch -format uid | datamash min 1 \
         | paste <(echo $var) - \
         >> ${outdir}/pmid.info
