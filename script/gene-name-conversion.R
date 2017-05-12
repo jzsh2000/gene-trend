f82fc0640d9399130f85432213fab1595fefb4d0
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-12 15:57:50 +0800

    Create R objects

--- script/gene-name-conversion.R
+++ script/gene-name-conversion.R
@@ -2,17 +2,19 @@
 library(tidyverse)
 library(stringr)
 
+# ========== prepare directory
+dir.create('data/current/robj', showWarnings = FALSE)
+
 # ===== process human gene
-order <- as.integer(readLines("data/current/Homo_sapiens.gene_order"))
-order <- data_frame(
-    GeneID = order,
-    order = seq(length(order))
-)
+order <- read_tsv("data/current/Homo_sapiens.gene_order") %>%
+    select(GeneID) %>%
+    mutate(order = row_number())
+
 gene_info <- read_tsv("data/current/Homo_sapiens.gene_info") %>%
     select(-1) %>%
     inner_join(as.tibble(order), by = c('GeneID' = 'GeneID')) %>%
-    arrange(order) %>%
     group_by(Symbol) %>%
+    arrange(order) %>%
     slice(1) %>%
     ungroup() %>%
     arrange(order) %>%
@@ -26,8 +28,14 @@ symbol2id <- gene_info %>%
 ensembl <- str_extract_all(gene_info$dbXrefs, '(?<=Ensembl:)ENSG[0-9]*')
 ensembl2id <- data_frame(
     Ensembl = unlist(ensembl),
-    GeneID = rep(gene_info$GeneID, sapply(ensembl, length))
-)
+    GeneID = rep(gene_info$GeneID, sapply(ensembl, length)),
+    order = rep(gene_info$order, sapply(ensembl, length))
+) %>%
+    group_by(Ensembl) %>%
+    arrange(order) %>%
+    slice(1) %>%
+    ungroup() %>%
+    select(-order)
 
 synonym <- str_split(gene_info$Synonyms, '\\|')
 synonym2id <- data_frame(
@@ -40,17 +48,23 @@ synonym2id <- data_frame(
     filter(n() == 1) %>%
     ungroup()
 
+save(gene_info, symbol2id, synonym2id, ensembl2id,
+     file = 'data/current/robj/human.RData')
+human.id = c(gene_info$GeneID,
+             symbol2id$Symbol,
+             ensembl2id$Ensembl,
+             synonym2id$Synonym)
+
 # ===== process mouse gene
-order <- as.integer(readLines("data/current/Mus_musculus.gene_order"))
-order <- data_frame(
-    GeneID = order,
-    order = seq(length(order))
-)
+order <- read_tsv("data/current/Mus_musculus.gene_order") %>%
+    select(GeneID) %>%
+    mutate(order = row_number())
+
 gene_info <- read_tsv("data/current/Mus_musculus.gene_info") %>%
     select(-1) %>%
     inner_join(as.tibble(order), by = c('GeneID' = 'GeneID')) %>%
-    arrange(order) %>%
     group_by(Symbol) %>%
+    arrange(order) %>%
     slice(1) %>%
     ungroup() %>%
     arrange(order) %>%
@@ -64,8 +78,14 @@ symbol2id <- gene_info %>%
 ensembl <- str_extract_all(gene_info$dbXrefs, '(?<=Ensembl:)ENSMUSG[0-9]*')
 ensembl2id <- data_frame(
     Ensembl = unlist(ensembl),
-    GeneID = rep(gene_info$GeneID, sapply(ensembl, length))
-)
+    GeneID = rep(gene_info$GeneID, sapply(ensembl, length)),
+    order = rep(gene_info$order, sapply(ensembl, length))
+) %>%
+    group_by(Ensembl) %>%
+    arrange(order) %>%
+    slice(1) %>%
+    ungroup() %>%
+    select(-order)
 
 synonym <- str_split(gene_info$Synonyms, '\\|')
 synonym2id <- data_frame(
@@ -77,3 +97,14 @@ synonym2id <- data_frame(
     group_by(Synonym) %>%
     filter(n() == 1) %>%
     ungroup()
+
+save(gene_info, symbol2id, synonym2id, ensembl2id,
+     file = 'data/current/robj/mouse.RData')
+mouse.id = c(gene_info$GeneID,
+             symbol2id$Symbol,
+             ensembl2id$Ensembl,
+             synonym2id$Synonym)
+
+# ==========
+write_rds(set_names(list(human.id, mouse.id), c('human', 'mouse')),
+          path = 'data/current/robj/id.rds')
