38ef72102b805339cb10f356596a1e406d416ddb
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-19 16:02:55 +0800

    Get human and mouse gene2pubmed information

--- script/split-gene-info.sh
+++ script/split-gene-info.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)/..
-zcat data/current/gene_info.gz \
-    | awk 'NR == 1 || $1 == "9606"' \
-    > data/current/Homo_sapiens.gene_info
 
-zcat data/current/gene_info.gz \
-    | awk 'NR == 1 || $1 == "10090"' \
-    > data/current/Mus_musculus.gene_info
+if [ ! -f data/current/Homo_sapiens.gene_info ]; then
+    echo 'Homo_sapiens.gene_info'
+    zcat data/current/gene_info.gz \
+        | awk 'NR == 1 || $1 == "9606"' \
+        > data/current/Homo_sapiens.gene_info
+fi
+
+if [ ! -f data/current/Mus_musculus.gene_info ]; then
+    echo 'Mus_musculus.gene_info'
+    zcat data/current/gene_info.gz \
+        | awk 'NR == 1 || $1 == "10090"' \
+        > data/current/Mus_musculus.gene_info
+fi
+
+if [ ! -f data/current/Homo_sapiens.gene2pubmed ]; then
+    echo 'Homo_sapiens.gene2pubmed'
+    zcat data/current/gene2pubmed.gz \
+        | awk 'NR == 1 || $1 == "9606"' \
+        > data/current/Homo_sapiens.gene2pubmed
+fi
+
+if [ ! -f data/current/Mus_musculus.gene2pubmed ]; then
+    echo 'Mus_musculus.gene2pubmed'
+    zcat data/current/gene2pubmed.gz \
+        | awk 'NR == 1 || $1 == "10090"' \
+        > data/current/Mus_musculus.gene2pubmed
+fi
