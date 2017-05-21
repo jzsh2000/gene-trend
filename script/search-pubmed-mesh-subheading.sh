3b648c3c881d14d8c1917b4e3b0bdfb8df3eff32
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-21 14:16:36 +0800

    Add script

--- script/search-pubmed-mesh-subheading.sh
+++ script/search-pubmed-mesh-subheading.sh
@@ -3,6 +3,6 @@ set -ueo pipefail
 
 query_string="$1"
 output_file=$(echo "$query_string" | tr ' ' '-').txt
-esearch -db pubmed -query "${query_string}[mesh]" \
+esearch -db pubmed -query "${query_string}[MeSH Subheading]" \
     | efetch -format uid \
     > $output_file
