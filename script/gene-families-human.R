3286469ec0858aca1df5ac1d2ddb9c17bfe9a9ab
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-28 10:48:16 +0800

    Write human gene group information

--- script/gene-families-human.R
+++ script/gene-families-human.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(stringr)
+library(forcats)
 load("data/current/robj/human.RData")
 
 gene_name_pattern = '^[A-Z]+[0-9]+[A-Za-z]?$'
@@ -31,7 +32,43 @@ id_candidate = bind_rows(symbol2id_candidate, synonym2id_candidate) %>%
     left_join(symbol2id, by = c('id' = 'GeneID')) %>%
     rename(name_official = Symbol, genes_alike = gene_count) %>%
     select(-official_name) %>%
+    mutate(name_official_lgl = (name == name_official)) %>%
     left_join(gene_info %>% select(GeneID, description, type_of_gene),
-         by = c('id' = 'GeneID'))
+         by = c('id' = 'GeneID')) %>%
+    replace_na(list(description = ''))
 
 write_rds(id_candidate, path = 'data/current/robj/human.gene-group.rds')
+
+# get description of gene groups
+group_stat = id_candidate %>%
+    select(root, genes_alike) %>%
+    unique() %>%
+    arrange(desc(genes_alike)) %>%
+    mutate(description = map_chr(root, function(group_name) {
+        description_all = id_candidate %>%
+            filter(root == group_name, name_official_lgl) %>%
+            pull(description)
+        if (length(description_all) == 0) {return('')}
+
+        description_max_length = max(str_length(description_all))
+        description_header = ''
+        for (i in seq(description_max_length)) {
+            description_header_df = str_sub(description_all, end = i) %>%
+                fct_count(sort = TRUE) %>%
+                mutate(prop = n / sum(n))
+            if ((description_header_df %>% slice(1) %>% pull(prop)) < .8) {
+                description_header = description_header_df %>%
+                    slice(1) %>%
+                    pull(f) %>%
+                    as.character() %>%
+                    str_sub(end = -2) %>%
+                    str_trim() %>%
+                    str_replace(',$', '')
+                break
+            }
+        }
+        print(description_header)
+        return(description_header)
+    }))
+
+write_rds(group_stat, path = 'data/current/robj/human.gene-group.stat.rds')
