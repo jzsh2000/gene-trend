a2087f9bf2568d35a4867431f79d12b92a9aba71
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-30 02:09:57 +0800

    Get pubmed-gene-mesh.txt file

--- script/get-gene-mesh-matrix.sh
+++ script/get-gene-mesh-matrix.sh
@@ -13,3 +13,41 @@ if [ ! -f $outdir/blacklist.pmid ]; then
         | sort \
        > $outdir/blacklist.pmid
 fi
+
+tmpfile=$(mktemp)
+echo "=== temp file: $tmpfile"
+
+# pubmed_id | txid | gene_id
+if [ ! -f $outdir/pubmed-gene.txt ]; then
+    echo "Generating $outdir/pubmed-gene.txt"
+    zcat data/current/gene2pubmed.gz \
+        | tail -n+2 \
+        | awk 'BEGIN{OFS="\t"}{print $3,$1,$2}' \
+        | sort -k1b,1 \
+        > $outdir/pubmed-gene.txt
+fi
+
+if [ ! -f $outdir/pubmed-gene.filtered.txt ]; then
+    echo "Get valid pubmed id"
+    cat $outdir/pubmed-gene.txt \
+        | cut -f1 \
+        | uniq \
+        | comm -23 - $outdir/blacklist.pmid \
+        > $tmpfile
+
+    # pubmed id can be mapped to 99 genes at maximal
+    echo "Generating $outdir/pubmed-gene.filtered.txt"
+    join $tmpfile $outdir/pubmed-gene.txt \
+        > $outdir/pubmed-gene.filtered.txt
+fi
+
+rm $tmpfile
+
+if [ ! -f data/pubmed/data/pubmed/clean_data/pubmed-mesh.txt ]; then
+    echo 'ERROR: please generate "pubmed-mesh.txt" file first'
+else
+    echo "Generating $outdir/pubmed-gene-mesh.txt"
+    join $outdir/pubmed-gene.filtered.txt \
+        data/pubmed/data/pubmed/clean_data/pubmed-mesh.txt \
+        > $outdir/pubmed-gene-mesh.txt
+fi
