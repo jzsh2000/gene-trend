4260c3a084b498a7dc41981d42426b7c2bb8c3fc
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-09-22 17:55:18 +0800

    Add new gene list

--- shiny/gene/script/gene-name-to-id.R
+++ shiny/gene/script/gene-name-to-id.R
@@ -1,11 +1,18 @@
 library(tidyverse)
+library(stringr)
 
-gene_info <- read_tsv('../../data/current/Homo_sapiens.gene_info')
-data_frame(Symbol = read_lines('gene-list/surface-marker.txt')) %>%
-    left_join(gene_info) %>%
-    group_by(Symbol) %>%
-    summarise(GeneID = min(GeneID)) %>%
-    mutate(GeneID = as.integer(GeneID)) %>%
-    select(GeneID, Symbol) %>%
-    arrange(GeneID) %>%
-    write_rds('gene-list/surface-marker.rds')
+load('robj/human.RData')
+# gene_info <- read_tsv('../../data/current/Homo_sapiens.gene_info')
+
+walk(Sys.glob('gene-list/*.txt'), function(gene_list_file) {
+    gene_list_name = str_extract(basename(gene_list_file), '.*(?=.txt)')
+    data_frame(Symbol = read_lines(gene_list_file)) %>%
+        left_join(gene_info) %>%
+        group_by(Symbol) %>%
+        summarise(GeneID = min(GeneID)) %>%
+        mutate(GeneID = as.integer(GeneID)) %>%
+        select(GeneID, Symbol) %>%
+        arrange(GeneID) %>%
+        write_rds(file.path(dirname(gene_list_file),
+                            paste0(gene_list_name, '.rds')))
+})
