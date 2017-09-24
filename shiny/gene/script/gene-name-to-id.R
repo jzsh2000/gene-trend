5304bbe2ce68a94fa47ae431cf9d92817e69c9f1
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-09-24 23:23:46 +0800

    Check the existance of file before writing

--- shiny/gene/script/gene-name-to-id.R
+++ shiny/gene/script/gene-name-to-id.R
@@ -1,18 +1,26 @@
 library(tidyverse)
 library(stringr)
 
+if (basename(getwd()) == 'gene-trend') {
+    setwd('shiny/gene')
+}
 load('robj/human.RData')
 # gene_info <- read_tsv('../../data/current/Homo_sapiens.gene_info')
 
 walk(Sys.glob('gene-list/*.txt'), function(gene_list_file) {
     gene_list_name = str_extract(basename(gene_list_file), '.*(?=.txt)')
-    data_frame(Symbol = read_lines(gene_list_file)) %>%
-        left_join(gene_info) %>%
-        group_by(Symbol) %>%
-        summarise(GeneID = min(GeneID)) %>%
-        mutate(GeneID = as.integer(GeneID)) %>%
-        select(GeneID, Symbol) %>%
-        arrange(GeneID) %>%
-        write_rds(file.path(dirname(gene_list_file),
-                            paste0(gene_list_name, '.rds')))
+    gene_rds_file = file.path(dirname(gene_list_file), paste0(gene_list_name, '.rds'))
+
+    print(gene_list_name)
+    if (!file.exists(gene_rds_file)) {
+        data_frame(Symbol = read_lines(gene_list_file)) %>%
+            left_join(gene_info) %>%
+            group_by(Symbol) %>%
+            summarise(GeneID = min(GeneID)) %>%
+            mutate(GeneID = as.integer(GeneID)) %>%
+            select(GeneID, Symbol) %>%
+            arrange(GeneID) %>%
+            write_rds(file.path(dirname(gene_list_file),
+                                paste0(gene_list_name, '.rds')))
+    }
 })
